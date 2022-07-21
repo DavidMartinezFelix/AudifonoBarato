40200ec191fdcbb8699c730862207784be65b639
david martinez <davidfelix2000@hotmail.com>
2022-07-21 16:33:07 +0200

    nueva home

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+let menus = document.querySelector{'#menus'};
+
+let menu_bar = document.querySelector('#menubar');
+
+    menu_bar.addEventListener('clic',function(){
+
+        menus.classlist.toogle('menu_toogle');
+    })
