0ea8b47480d82a0442486944222dc3859fdae18f
david martinez <davidfelix2000@hotmail.com>
2022-07-30 17:05:11 +0200

    first-commit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-let menus = document.querySelector{'#menus'};
-
-let menu_bar = document.querySelector('#menubar');
-
-    menu_bar.addEventListener('clic',function(){
-
-        menus.classlist.toogle('menu_toogle');
-    })
